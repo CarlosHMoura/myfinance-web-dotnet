d69a40d3e0b0eff39462a8b68399df6fa65486ad
CarlosHMoura <carlosmoura@live.com>
2023-12-10 20:13:19 -0300

    Create Project

---
+++ scripts/Create Tables.sql
@@ -0,0 +1,37 @@
+USE myfinance
+GO
+
+CREATE TABLE [dbo].[PlanoConta](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descricao] [nvarchar](max) NOT NULL,
+	[Tipo] [int] NOT NULL,
+	[Ativo] [bit] NOT NULL,
+ CONSTRAINT [PK_PlanoConta] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Transacao](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Historico] [nvarchar](max) NULL,
+	[Tipo] [nvarchar](max) NULL,
+	[Valor] [decimal](16, 4) NOT NULL,
+	[PlanoContaId] [int] NOT NULL,
+	[Data] [datetime] NOT NULL,
+	[Ativo] [bit] NOT NULL,
+ CONSTRAINT [PK_Transacao] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Transacao]  WITH CHECK ADD  CONSTRAINT [FK_Transacao_PlanoConta_PlanoContaId] FOREIGN KEY([PlanoContaId])
+REFERENCES [dbo].[PlanoConta] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[Transacao] CHECK CONSTRAINT [FK_Transacao_PlanoConta_PlanoContaId]
+GO

f94f9ad4ca7d8d6ce6156d3952ac3168c635bab9
CarlosHMoura <carlosmoura@live.com>
2023-12-10 20:29:33 -0300

    Add Models and Index Account Plan

---
+++ myfinance-web-dotnet/Models/AccountPlanModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using myfinance_web_dotnet.Enumerations;
+
+namespace myfinance_web_dotnet.Models
+{
+    public record AccountPlanModel
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public TypeAccountEnum Type { get; set; }
+        public bool Active { get; set; }
+        public List<SelectListItem> AllTypes
+        {
+            get
+            {
+                return Enum.GetValues(typeof(TypeAccountEnum))
+                           .Cast<TypeAccountEnum>()
+                           .Select(t => new SelectListItem
+                           {
+                               Value = t.ToString(),
+                               Text = t.ToString()
+                           })
+                           .ToList();
+            }
+        }
+    }
+}
---
+++ myfinance-web-dotnet/Models/TransactionModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace myfinance_web_dotnet.Models
+{
+    public record TransactionModel
+    {
+        public int Id { get; set; }
+        public string? History { get; set; }
+        public string? Type { get; set; }
+        public decimal? Value { get; set; }
+        public DateTime Date { get; set; }
+        public int AccountPlanId { get; set; }
+        public bool? Active { get; set; }
+        public List<AccountPlanModel> AccountsPlan { get; set; } = new List<AccountPlanModel>();
+
+        public List<SelectListItem> TypesAccountPlan
+        {
+            get
+            {
+                return AccountsPlan
+                            .Select(t => new SelectListItem
+                            {
+                                Value = t.Id.ToString(),
+                                Text = t.Description
+                            })
+                            .ToList();
+
+            }
+
+        }
+    }
+}
